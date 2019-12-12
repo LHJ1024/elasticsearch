1f6aebdd4d3e44bd8ca93631f5b5e6230e6c7d8b
lhj1024 <18875143382@163.com>
2019-12-12 15:01:07 +0800

    使用jestclient操作es基础增删改查。没有看到聚合，和其他特别DSL操作。

--- src/test/java/com/springboot/elasticsearch/ElasticsearchApplicationTests.java
+++ src/test/java/com/springboot/elasticsearch/ElasticsearchApplicationTests.java
@@ -2,7 +2,10 @@ package com.springboot.elasticsearch;
 
 import com.springboot.elasticsearch.bean.Article;
 import io.searchbox.client.JestClient;
+import io.searchbox.core.Delete;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -11,12 +14,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
- public  class ElasticsearchApplicationTests {
+public class ElasticsearchApplicationTests {
     @Autowired
     JestClient jestClient;
-
+//这个是直接创建了index和带有文档内容
     @Test
     public void contextLoads() {
         Article article = new Article();
@@ -28,6 +32,38 @@ import java.io.IOException;
         try {
             //执行
             jestClient.execute(index);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+//查询符合hellodoc内容
+    @Test
+    public void search() {
+        String json = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"match\" : {\n" +
+                "            \"content\" : \"hello\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+        Search search = new Search.Builder(json).addIndex("ballshit").addType("dickman").build();
+        try {
+            SearchResult result = jestClient.execute(search);
+            System.out.println(result.getJsonString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+//删除文档根据文档Id
+    @Test
+    public void delete() {
+
+        try {
+            jestClient.execute(new Delete.Builder("1")
+                    .index("ballshit")
+                    .type("dickman")
+                    .build());
         } catch (IOException e) {
             e.printStackTrace();
         }
